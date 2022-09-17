ad1e845fa13f5bb5645757d9ab09226620d58467
Angularistic <angularistic@gmail.com>
2022-09-17 13:54:47 +0530

    Update to Angular 14

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # Sidenav
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.7.
-
-### For version 14.0.0 visit [here](https://github.com/Angular-istic/sidenav/tree/sidenav_ng-14)
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.2.
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
 ## Code scaffolding
 
--- src/app/sidenav/nav-data.ts
+++ src/app/sidenav/nav-data.ts
@@ -34,29 +34,4 @@ export const navbarData = [
         icon: 'fal fa-cog',
         label: 'Settings'
     },
-    {
-        routeLink: '',
-        icon: 'fal fa-cog',
-        label: 'Menu with Children',
-        submenuLevel1: [
-            {
-                routeLink: '',
-                icon: 'fal fa-cog', // icon if needed
-                label: 'Sub menu level 1',
-            },
-            {
-                routeLink: '',
-                icon: 'fal fa-cog', // icon if needed
-                label: 'Sub menu level 1',
-                submenuLevel2: [
-                    {
-                        routeLink: '',
-                        icon: 'fal fa-cog', // icon if needed
-                        label: 'Sub menu level 2',
-                    }
-                ]
-            }
-        ]
-    },
-
 ];
